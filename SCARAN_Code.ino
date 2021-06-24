ca4412a64174fa54066b6b9a9b4d2487fd09faa5
MOHAMMED SAIF <44542705+mohammed-saif@users.noreply.github.com>
2021-06-24 22:16:28 +0530

    Update SCARAN_Code.ino

--- SCARAN_Code.ino
+++ SCARAN_Code.ino
@@ -9,8 +9,6 @@ Servo servo04;
 Servo servo05;
 Servo servo06;
 
-
-
 int servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; // current position
 int servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; // previous position
 int servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50]; // for storing positions/steps
